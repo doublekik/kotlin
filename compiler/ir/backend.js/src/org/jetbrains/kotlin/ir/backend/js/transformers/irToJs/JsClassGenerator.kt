1144d81cb95bb38194f7363950412d8c99714675
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-09-20 22:45:00 +0200

    fix: most of ES classes tests.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsClassGenerator.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsClassGenerator.kt
@@ -214,6 +214,10 @@ class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationCo
 
         context.staticContext.classModels[irClass.symbol] = classModel
 
+        if (irClass.hasAnnotation(JsAnnotations.MetaClass)) {
+            context.staticContext.metaClasses.add(irClass.symbol)
+        }
+
         return classBlock
     }
 
@@ -321,7 +325,7 @@ class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationCo
         val setMetadataFor = context.staticContext.backendContext.intrinsics.setMetadataForSymbol.owner
 
         val ctor = classNameRef
-        val parent = baseClassRef
+        val parent = baseClassRef?.takeIf { !es6mode }
         val name = generateSimpleName()
         val interfaces = generateInterfacesList()
         val metadataConstructor = getMetadataConstructor()
@@ -385,7 +389,7 @@ class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationCo
             .distinct()
             .map { JsIntLiteral(it) }
 
-        return JsArrayLiteral(arity)
+        return JsArrayLiteral(arity).takeIf { arity.isNotEmpty() }
     }
 
     private fun generateAssociatedObjectKey(): JsIntLiteral? {
