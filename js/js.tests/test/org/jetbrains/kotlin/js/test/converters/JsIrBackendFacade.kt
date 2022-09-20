1f139c1e9aa874bc1e106a161b9f6936a5119ffc
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-09-20 18:20:36 +0200

    test: add test run for ES6 classes.

--- js/js.tests/test/org/jetbrains/kotlin/js/test/converters/JsIrBackendFacade.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/converters/JsIrBackendFacade.kt
@@ -82,6 +82,7 @@ class JsIrBackendFacade(
         val perModule = JsEnvironmentConfigurationDirectives.PER_MODULE in module.directives
         val runNewIr2Js = JsEnvironmentConfigurationDirectives.RUN_NEW_IR_2_JS in module.directives
         val keep = module.directives[JsEnvironmentConfigurationDirectives.KEEP].toSet()
+        val es6Mode = JsEnvironmentConfigurationDirectives.ES_CLASSES in module.directives
 
         val granularity = when {
             !firstTimeCompilation -> JsGenerationGranularity.WHOLE_PROGRAM
@@ -148,7 +149,7 @@ class JsIrBackendFacade(
             exportedDeclarations = setOf(FqName.fromSegments(listOfNotNull(testPackage, TEST_FUNCTION))),
             keep = keep,
             dceRuntimeDiagnostic = null,
-            es6mode = false,
+            es6mode = es6Mode,
             baseClassIntoMetadata = false,
             safeExternalBoolean = JsEnvironmentConfigurationDirectives.SAFE_EXTERNAL_BOOLEAN in module.directives,
             safeExternalBooleanDiagnostic = module.directives[JsEnvironmentConfigurationDirectives.SAFE_EXTERNAL_BOOLEAN_DIAGNOSTIC].singleOrNull(),
